4d68b954a71d30cffcf631d9cbf3f3b20f932ed8
kasiabeben10 <96909263+kasiabeben10@users.noreply.github.com>
2025-01-17 20:29:26 +0100

    some changes in dish edit and dish add form usage

--- frontend/src/pages/Dashboard/DishEditPage.tsx
+++ frontend/src/pages/Dashboard/DishEditPage.tsx
@@ -1,7 +1,14 @@
 import { Flex, Typography } from 'antd';
-import DishEditForm from '../../components/dishes/DishEdit/DishEditForm.tsx';
+import DishForm from '../../components/dishes/DishEdit/DishForm.tsx';
+import { useActions } from '../../store/hooks.ts';
+import { useParams } from 'react-router';
+import {useUpdateDish} from "../../api/queries/dishes.ts";
 
 const DishEditPage = () => {
+  const { notificationSend } = useActions();
+  const { id } = useParams<{ id: string }>();
+  const mutate = useUpdateDish(notificationSend, id!).mutate;
+
   return (
     <Flex
       vertical
@@ -10,7 +17,7 @@ const DishEditPage = () => {
       style={{ width: '100%', marginTop: '2rem' }}
     >
       <Typography.Title level={2}>Edit dish</Typography.Title>
-      <DishEditForm />
+      <DishForm mutate={mutate}/>
     </Flex>
   );
 };
