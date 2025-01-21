7592047116c869fc355e68524c64a6578040e523
kasiabeben10 <96909263+kasiabeben10@users.noreply.github.com>
2025-01-21 12:46:12 +0100

    add manage-accout-page alignment

--- frontend/src/pages/Dashboard/ManageYourAccount.tsx
+++ frontend/src/pages/Dashboard/ManageYourAccount.tsx
@@ -17,21 +17,27 @@ const ManageYourAccount: React.FC = () => {
   };
 
   return (
-    <div style={{ padding: '20px' }}>
-      <Typography.Title
-        level={2}
-        style={{ textAlign: 'center', marginBottom: '20px' }}
-      >
-        Manage Your Account
-      </Typography.Title>
-      <Typography.Text>
-        <Paragraph> Welcome, {username}</Paragraph>
-      </Typography.Text>
-      <Button type="primary" onClick={handleButtonClick}>
-        Change Password
-      </Button>
-      {showForm && <ChangePasswordForm />}
-    </div>
+      <div style={{padding: '20px'}}>
+          <Typography.Title
+              level={2}
+              style={{textAlign: 'center', marginBottom: '20px'}}
+          >
+              Manage Your Account
+          </Typography.Title>
+          <Typography.Text>
+              <Paragraph style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+              }}> Welcome, {username}</Paragraph>
+          </Typography.Text>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <Button type="primary" onClick={handleButtonClick}>
+                  Change Password
+              </Button>
+          </div>
+          {showForm && <ChangePasswordForm/>}
+      </div>
   );
 };
 
